0d3d8cfbce18bc346b06acc1cb02a0c9465b91e9
Jashan Chopra <jashanxchopra@gmail.com>
2019-03-07 15:35:09 -0700

    Created new folder for Yearly NMHC Comparison Plots, added first script

---
+++ analyses/nmhc_YearlyComparisons/FileInput.py
@@ -0,0 +1,12 @@
+"""
+Created on Thursday, March 7th, 2019.
+
+This script serves to create plots of each of the NMHC compounds
+over the course of 365 days. Each year of data is plotted as a separate line.
+
+Goals:
+1) Times on the x axis should be written as day of the year instead of decimal
+2) Each plot should have a proper legend
+3) Remove outliers from the data sets
+4) [Maybe?] Put each compound graph in one larger subplot for trend comparison
+"""
