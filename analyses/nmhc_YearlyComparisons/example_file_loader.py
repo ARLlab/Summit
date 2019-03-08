201faa1912981d78b9ef91646f4a7a9df4a48686
Brendan Blanchard <brendano257@gmail.com>
2019-03-08 11:35:24 -0700

    Update example_file_loader.py

--- analyses/nmhc_YearlyComparisons/example_file_loader.py
+++ analyses/nmhc_YearlyComparisons/example_file_loader.py
@@ -1,4 +1,4 @@
-def load_file():  # define a function to load your data
+def load_file(filename):  # define a function to load your data, one input parameter is the name of the file to be loaded
   import pandas as pd
-  data = pd.read_csv('my_fake_file.csv') # load file within the function
+  data = pd.read_csv(filename) # load file with given name inside the function
   return data  # tell function to return the loaded data
