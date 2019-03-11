116d462bd118fe2f3d3c8ff9156ea9d205f48f5a
Brendan Blanchard <brendano257@gmail.com>
2019-03-11 13:42:23 -0600

    Added DataFile object to allow file-checking, tested and working

--- processors/summit_picarro_processor/summit_picarro.py
+++ processors/summit_picarro_processor/summit_picarro.py
@@ -41,68 +41,34 @@ class TempDir():
 	def __exit__(self, *args):
 		os.chdir(self.old_dir)
 
-def connect_to_db(engine_str, directory):
-	"""
-	Example:
-	engine, session, Base = connect_to_db('sqlite:///reservoir.sqlite', dir)
 
-	Takes string name of the database to create/connect to, and the directory it should be in.
+class DataFile(Base):
 
-	engine_str: str, name of the database to create/connect to.
-	directory: str/path, directory that the database should be made/connected to in.
-		Requires context manager TempDir in order to work with async
-	"""
+	__tablename__ = 'files'
 
-	from summit_picarro import Base, TempDir
+	id = Column(Integer, primary_key=True)
+	_name = Column(String)
+	_path = Column(String)
+	size = Column(Integer)
 
-	from sqlalchemy import create_engine
-	from sqlalchemy.orm import sessionmaker
+	def __init__(self, path):
+		self.path = path
+		self.size = Path.stat(path).st_size
 
-	with TempDir(directory):
-		engine = create_engine(engine_str)
-	Session = sessionmaker(bind=engine)
-	sess = Session()
+	@property
+	def path(self):
+		return Path(self._path)
 
-	return engine, sess, Base
+	@path.setter
+	def path(self, value):
+		self._path = str(value)
+		self._name = value.name
 
-def configure_logger(rundir):
-	logfile = Path(rundir) / 'processor_logs/summit_picarro.log'
-	logger = logging.getLogger('summit_voc')
-	logger.setLevel(logging.DEBUG)
-	fh = logging.FileHandler(logfile)
-	fh.setLevel(logging.DEBUG)
-
-	ch = logging.StreamHandler()
-	ch.setLevel(logging.INFO)
-
-	formatter = logging.Formatter('%(asctime)s -%(levelname)s- %(message)s')
-
-	[H.setFormatter(formatter) for H in [ch, fh]]
-	[logger.addHandler(H) for H in [ch, fh]]
-
-	return logger
+	@property
+	def name(self):
+		return self._name
 
 
-logger = configure_logger(rundir)
-
-def list_files_recur(path):
-	files = []
-	for file in path.rglob('*'):
-		files.append(file)
-
-	return files
-
-def get_all_data_files(rundir_path):
-	"""
-	Recursively search the
-	:param rundir_path:
-	:return:
-	"""
-	data_path = rundir_path / 'data'
-	files = list_files_recur(data_path)
-	files[:] = [file for file in files if '.dat' in file.name]
-
-	return files
 
 class Datum(Base):
 
@@ -132,4 +98,73 @@ class Datum(Base):
 			setattr(self, var, line_dict.get(column_to_instance_names.get(var)))
 
 		self.date = datetime.fromtimestamp(line_dict.get('EPOCH_TIME'))
+
+
+def connect_to_db(engine_str, directory):
+	"""
+	Example:
+	engine, session, Base = connect_to_db('sqlite:///reservoir.sqlite', dir)
+
+	Takes string name of the database to create/connect to, and the directory it should be in.
+
+	engine_str: str, name of the database to create/connect to.
+	directory: str/path, directory that the database should be made/connected to in.
+		Requires context manager TempDir in order to work with async
+	"""
+
+	from summit_picarro import Base, TempDir
+
+	from sqlalchemy import create_engine
+	from sqlalchemy.orm import sessionmaker
+
+	with TempDir(directory):
+		engine = create_engine(engine_str)
+	Session = sessionmaker(bind=engine)
+	sess = Session()
+
+	return engine, sess, Base
+
+
+def configure_logger(rundir):
+	logfile = Path(rundir) / 'processor_logs/summit_picarro.log'
+	logger = logging.getLogger('summit_voc')
+	logger.setLevel(logging.DEBUG)
+	fh = logging.FileHandler(logfile)
+	fh.setLevel(logging.DEBUG)
+
+	ch = logging.StreamHandler()
+	ch.setLevel(logging.INFO)
+
+	formatter = logging.Formatter('%(asctime)s -%(levelname)s- %(message)s')
+
+	[H.setFormatter(formatter) for H in [ch, fh]]
+	[logger.addHandler(H) for H in [ch, fh]]
+
+	return logger
+
+
+logger = configure_logger(rundir)
+
+
+def list_files_recur(path):
+	files = []
+	for file in path.rglob('*'):
+		files.append(file)
+
+	return files
+
+
+def get_all_data_files(rundir_path):
+	"""
+	Recursively search the
+	:param rundir_path:
+	:return:
+	"""
+	data_path = rundir_path / 'data'
+	files = list_files_recur(data_path)
+	files[:] = [file for file in files if '.dat' in file.name]
+
+	return files
+
+
 
