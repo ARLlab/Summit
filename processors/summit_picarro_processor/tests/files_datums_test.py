bd811071eacf8a2ee6fc8786b5fe06c79e6c6612
Brendan Blanchard <brendano257@gmail.com>
2019-03-11 13:42:50 -0600

    Added testing for DataFile objects; working

--- processors/summit_picarro_processor/tests/files_datums_test.py
+++ processors/summit_picarro_processor/tests/files_datums_test.py
@@ -1,7 +1,7 @@
 import os, random
 from pathlib import Path
 import pandas as pd
-from summit_picarro import get_all_data_files, connect_to_db, Datum
+from summit_picarro import get_all_data_files, connect_to_db, Datum, DataFile
 
 rundir = Path(r'C:\Users\arl\Desktop\summit_master\processors\summit_picarro_processor\tests')
 
@@ -19,6 +19,7 @@ while len(flist) < int(len(files)/10):
 
 print(f'{len(flist)} data files chosen at random.')
 
+file_list = []
 data_list = []
 for ind in flist:
 	df = pd.read_csv(files[ind], delim_whitespace=True)
@@ -27,6 +28,8 @@ for ind in flist:
 	for line in df_list:
 		data_list.append(Datum(line))
 
+	file_list.append(DataFile(files[ind]))
+
 print(f'{len(data_list)} Datum objects created.')
 
 for d in data_list:
@@ -34,6 +37,14 @@ for d in data_list:
 session.commit()
 
 print(f'{len(data_list)} Datum objects committed.')
+
+for file in file_list:
+	session.add(file)
+session.commit()
+
+print(f'{len(file_list)} DataFile objects committed.')
+
+
 
 
 
