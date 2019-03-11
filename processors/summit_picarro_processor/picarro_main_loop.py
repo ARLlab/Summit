8fb8a016216daea6e8563238d68a315c3d488a95
Brendan Blanchard <brendano257@gmail.com>
2019-03-11 13:41:58 -0600

    Framed needed pieces in docstring

--- processors/summit_picarro_processor/picarro_main_loop.py
+++ processors/summit_picarro_processor/picarro_main_loop.py
@@ -1,9 +1,29 @@
 import os, asyncio
 
-async def check_files():
-	pass
+"""
+Things Needed:
+
+File checking:
+	Needs to:
+		Check for new or updated files
+		Read new files
+		
+Calibration Handling:
+	Needs to:
+		Search all data, then new data
+		Pick out all calibration data
+		Separate into low, middle, high standards based on valves
+			Group in a single calibration event and calculate some things based on the regression, etc
+		Potentially apply information from the above on data
+		
+Plotting:
+	Needs to:
+		Plot data if new data is available
+		
+
+"""
 
 
-async def load_new_data():
+async def check_load_new_data():
 	pass
 
