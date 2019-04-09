9b8eb35ed7d10d36dfd5588c57ad7bf88a1eb0f1
Jashan Chopra <jashanxchopra@gmail.com>
2019-04-09 14:16:18 -0600

    file source fixes

--- analyses/past_ch4_integrations/monthlySort.py
+++ analyses/past_ch4_integrations/monthlySort.py
@@ -11,8 +11,6 @@ Created in Spyder 3.3.2 in Anaconda Distribution, Python 3.7
 ## Import Libraries
 import os
 import shutil
-import string
-import pandas as pd
 
 years = [2016, 2017, 2018]
 months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
@@ -26,8 +24,8 @@ for yrs in years:
         os.mkdir(path)
 
 # move files into respective folders
-source = r'C:\Users\ARL\Desktop\test'
-files = [file for file in os.scandir(source) if '.chr' in file.name]
+source = r'C:\Users\ARL\Desktop\pastch4BUP'
+files = [file for file in os.listdir(source) if '.chr' in file]
 
 from isleapyear import isleapyear
 
@@ -36,41 +34,51 @@ for f in files:                     # iterate over all the files
         for mo in months:           # iterate over months
             if f[:4] == '%i'%yr:    # if first four letters indicate yr
                 if isleapyear(yr):  # leap year julian dates are different than normal
-
-                    if f[5:7] >= 1 && f[5:7] <= 31:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    if int(f[5:7]) >= 1 and int(f[5:7]) <= 31:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 32 && f[5:7] <= 60:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 32 and int(f[5:7]) <= 60:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 61 && f[5:7] <= 91:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 61 and int(f[5:7]) <= 91:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 92 && f[5:7] <= 121:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 92 and int(f[5:7]) <= 121:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 122 && f[5:7] <= 152:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 122 and int(f[5:7]) <= 152:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 153 && f[5:7] <= 182:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 153 and int(f[5:7]) <= 182:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 183 && f[5:7] <= 213:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 183 and int(f[5:7]) <= 213:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 214 && f[5:7] <= 244:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 214 and int(f[5:7]) <= 244:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 245 && f[5:7] <= 274:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 245 and int(f[5:7]) <= 274:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 275 && f[5:7] <= 305:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 275 and int(f[5:7]) <= 305:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    elif: f[5:7] >= 306 && f[5:7] <= 335:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    elif int(f[5:7]) >= 306 and int(f[5:7]) <= 335:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                    else: f[5:7] >= 336 && f[5:7] <= 366:
-                        dest = r'C:\Users\ARL\Desktop\pastch4\%i'%yr
+                    else:
+                        dest = r'C:\Users\ARL\Desktop\pastch4\%i\%s'%(yr,mo)
+                        f = r'C:\Users\ARL\Desktop\pastch4BUP\%s'%(f)
                         shutil.move(f,dest)     # move the files
-                else:
