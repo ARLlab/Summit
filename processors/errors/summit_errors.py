05570de3848ec0c981d91ae8b374101d0fc1be1e
Brendan Blanchard <brendano257@gmail.com>
2019-04-09 13:51:48 -0600

    Errors and EmailTemplates tested and working; NewDataEmail not tested

--- processors/errors/summit_errors.py
+++ processors/errors/summit_errors.py
@@ -1,4 +1,4 @@
-import logging, os
+import logging, os, json, time
 from pathlib import Path
 from datetime import datetime
 import datetime as dt
@@ -10,58 +10,119 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
+from summit_core import configure_logger
+
 rundir = Path(r'C:\Users\arl\Desktop\summit_master\processors\errors')
+
+auth_file = (rundir / 'email_auth.json')
+sender = 'arl.lab.cu@gmail.com'
+
+logger = configure_logger(rundir, __name__)
 
 Base = declarative_base()  # needed to subclass for sqlalchemy objects
 
+test_list = ['brbl4762@colorado.edu']
 global_list = ['brbl4762@colorado.edu']
+processor_email_list = ['brbl4762@colorado.edu']
 
 
-def configure_logger(rundir):
-	"""
-	Create the project-specific logger. DEBUG and up is saved to the log, INFO and up appears in the console.
+class Error():
 
-	:param rundir: path to create log sub-path in
-	:return: logger object
-	"""
+	id = 0
 
-	logfile = Path(rundir) / 'processor_logs/summit_errors.log'
-	logger = logging.getLogger('summit_errors')
-	logger.setLevel(logging.DEBUG)
-	fh = logging.FileHandler(logfile)
-	fh.setLevel(logging.DEBUG)
+	def __init__(self, reason, resolution_function, resolution_value, email_template=None, expiration=None):
 
-	ch = logging.StreamHandler()
-	ch.setLevel(logging.INFO)
+		self.id = Error.id
+		Error.id += 1  # create unique IDs from incrementing class variable
 
-	formatter = logging.Formatter('%(asctime)s -%(levelname)s- %(message)s')
+		self.email_template = email_template
+		self.expiration = expiration
+		self.resolution_function = resolution_function
+		self.resolution_value = resolution_value
+		self.reason = '[' + reason + ']'
 
-	[H.setFormatter(formatter) for H in [ch, fh]]
-	[logger.addHandler(H) for H in [ch, fh]]
+		logger.error(f'Error for {self.reason} intiated with error ID: {self.id}')
+		self.email_template.send()
 
-	return logger
-
-
-logger = configure_logger(rundir)
-
-
-class Error(Base):
-	"""
-	Errors are activatable states that trigger logging, and usually emails.
-	Once activated, they send emails/log events and if/when resolved, will send a resolution email.
-
-	For instance, not having new data for 6 hours from the station might activate an error, send an email, and stay
-	active until new data is found. Once new data comes in, it will send an email indicating the error is resolved.
-	"""
-	__tablename__ = 'errors'
-
-	id = Column(Integer, primary_key=True)
-
-	def __init__(self, logger, reason, email_list=None, expiration=None):
-		pass
+	def check_resolution(self):
+		if self.resolution_function(self.resolution_value):
+			self.resolve()
+			return True
+		else:
+			logger.error(f'Error for {self.reason} with error ID {self.id} was checked and is un-resolved.')
+			return False
 
 	def resolve(self):
-		pass
+		self.email_template.send_resolution()
+
+
+class EmailTemplate():
+
+	def __init__(self, send_from, send_to_list, body, resolution_body='',
+				 subject=None, attachments=None, auth_file=auth_file):
+		self.send_from = send_from
+		self.send_to = send_to_list
+		self.subject = subject
+		self.body = body
+		self.resolution_body = resolution_body
+		self.attachments = attachments
+
+	def send(self):
+		try:
+			auth = json.loads(auth_file.read_text())
+			user, passw = (auth.get('gmail_username'), auth.get('gmail_password'))
+			send_email(self.send_from, self.send_to, self.subject,
+					   self.body, user, passw,
+					   attach=self.attachments)
+		except Exception as e:
+			print(e.args)
+			print('Handle the new exception!')
+
+	def send_resolution(self):
+		try:
+			auth = json.loads(auth_file.read_text())
+			user, passw = (auth.get('gmail_username'), auth.get('gmail_password'))
+			send_email(self.send_from, self.send_to, f'Resolved - {self.subject}',
+					   self.resolution_body, user, passw,
+					   attach=self.attachments)
+		except Exception as e:
+			print(e.args)
+			print('Handle the new exception!')
+
+
+class ProccessorEmail(EmailTemplate):
+
+	def __init__(self, send_from, processor_name, attachments=()):
+		subject = f'{processor_name} Error'
+		body = f'The {processor_name} failed to run properly at {datetime.now().isoformat(" ")}.'
+		send_to_list = processor_email_list
+
+		super().__init__(send_from, send_to_list, body, subject=subject, attachments=attachments)
+
+
+class NewDataEmail(EmailTemplate):
+
+	def __init__(self, send_from, processor_name, last_data_time, attachments=()):
+		subject = f'No New Data for {processor_name}'
+		body = f'There has been no new data for the {processor_name} in {datetime.now() - last_data_time}.'
+		send_to_list = processor_email_list
+
+
+		self.last_data_time = last_data_time
+		super().__init__(send_from, send_to_list, body, subject=subject, attachments=attachments)
+
+	def send_resolution(self):
+		try:
+			self.resolution_body = (f'New data for {self.processor_name}' +
+									f'was found after {datetime.now() - self.last_data_time}.')
+			auth = json.loads(auth_file.read_text())
+			user, passw = (auth.get('gmail_username'), auth.get('gmail_password'))
+			send_email(self.send_from, self.send_to, f'Resolved - {self.subject}',
+					   self.resolution_body, user, passw,
+					   attach=self.attachments)
+		except Exception as e:
+			print(e.args)
+			print('Handle the new exception!')
 
 
 def send_email(send_from, send_to, subject, body, user, passw, attach=None, server='smtp.gmail.com'):
@@ -98,19 +159,61 @@ def send_email(send_from, send_to, subject, body, user, passw, attach=None, serv
 		logger.critical('An email failed to send due to failed server connection.')
 
 
-def main(directory):
+def send_basic_w_attachement(directory):
 	import json
-	import os
-	filedata = (rundir / 'email_auth.txt').read_text()
-	userdata = json.loads(filedata)
+	userdata = json.loads(auth_file.read_text())
 
 	file_to_attach = directory / 'processor_logs/summit_errors.log'
 
-	send_email('arl.lab.cu@gmail.com', global_list, 'Error Test!',
+	send_email(sender, test_list, 'Error Test!',
 			   'This has been a scheduled test of the Summit Error Handling System.\n The entire log is attached.',
 			   userdata.get('gmail_username'), userdata.get('gmail_password'),
 			   attach=[file_to_attach])
 
 
+def main():
+
+
+	def basic_template_test():
+		err_email = EmailTemplate(sender, test_list, 'This is a class-based test.', subject='ClassTest')
+		err_email.send()
+		err_email.send_resolution()
+
+
+	def processor_template_test():
+		proc_email = ProccessorEmail(sender, 'Picarro Processor')
+		proc_email.send()
+		proc_email.send_resolution()
+
+
+	def error_and_resolution_test():
+
+		def is_it_time_test(the_time):
+			if datetime.now() > the_time:
+				logger.error("It's finally time!")
+				return True
+			else:
+				return False
+
+		template = ProccessorEmail('arl.lab.cu@gmail.com', 'Time Processor')
+		the_time = datetime.now() + dt.timedelta(seconds=30)
+		test_error = Error("it's not time yet", is_it_time_test, the_time, email_template=template)
+
+		for i in range(8):
+			if test_error.check_resolution():
+				del test_error
+				break
+
+			time.sleep(5)
+
+
+	# send_basic_w_attachement(rundir)  # works
+	# basic_template_test()  # works
+	# processor_template_test()  # works
+	# error_and_resolution_test()  # works
+
+	pass
+
+
 if __name__ == '__main__':
-	main(rundir)
+	main()
