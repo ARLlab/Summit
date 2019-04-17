0f39c73264f7ec8fe72b6a4d2af8b6f1e0bb3aa9
Brendan Blanchard <brendano257@gmail.com>
2019-04-17 11:22:13 -0600

    Set up to run methane, voc, and errors to test

--- core/summit.py
+++ core/summit.py
@@ -1,9 +1,20 @@
 """
 This will be the main script for all processing.
 
-TODO: Remove testing while loops from all async processes
-TODO: All async processing functions should be modularized with imports etc internal to functions.
-TODO: All async proceses need additional params to be passed necessary data?
-
 TODO: Flow chart for processes
 """
+from voc_main_loop import main as voc_processor
+from methane_main_loop import main as methane_processor
+from error_main_loop import main as error_processor
+import asyncio
+
+async def main():
+    await asyncio.create_task(voc_processor())
+    await asyncio.create_task(methane_processor())
+    await asyncio.create_task(error_processor())
+    await asyncio.sleep(15*60)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
