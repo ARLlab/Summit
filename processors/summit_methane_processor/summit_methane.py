1330096d75ea5c53343ee9345d630091b01643ba
Brendan Blanchard <brendano257@gmail.com>
2019-04-19 10:30:52 -0600

    summit_methane_plot() now returns plotname

--- processors/summit_methane_processor/summit_methane.py
+++ processors/summit_methane_processor/summit_methane.py
@@ -490,5 +490,9 @@ def summit_methane_plot(dates, compound_dict, limits=None, minor_ticks=None, maj
 
     f1.subplots_adjust(bottom=.20)
 
-    f1.savefig(f'{fn_list}_last_week.png', dpi=150)
+    plot_name = f'{fn_list}_last_week.png'
+
+    f1.savefig(plot_name, dpi=150)
     plt.close(f1)
+
+    return plot_name
