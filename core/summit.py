9b5e048a900770fe234d452c08e0e33c80e33ef5
Brendan Blanchard <brendano257@gmail.com>
2019-05-03 11:46:21 -0600

    Moved excel loading to main() in voc_main_loop

--- core/summit.py
+++ core/summit.py
@@ -10,16 +10,12 @@ for d in processor_dirs:
 	sys.path.append(str(d))
 
 from voc_main_loop import main as voc_processor
-from voc_main_loop import load_excel_corrections
 from methane_main_loop import main as methane_processor
 from picarro_main_loop import main as picarro_processor
 from error_main_loop import check_for_new_data, check_existing_errors
 from summit_core import check_send_plots
 from summit_errors import send_processor_email
 import asyncio
-
-ambient_sheetpath = r'Z:\Data\Summit_GC\Summit_GC_2019\NMHC_results\Ambient_2019.xlsx'
-# TODO include in JSON filepaths instead
 
 
 async def main():
@@ -42,9 +38,6 @@ async def main():
 		vocs = await asyncio.create_task(voc_processor())
 		methane = await asyncio.create_task(methane_processor())
 		picarro = await asyncio.create_task(picarro_processor())
-
-		# excel_loads = await asyncio.create_task(load_excel_corrections(ambient_sheetpath, logger))
-		# TODO : Change to call only once after 12:30AM each day (and config-store a date for plots?)
 
 		if vocs or methane or picarro:
 			await asyncio.create_task(check_send_plots(logger))
