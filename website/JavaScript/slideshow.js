8eec754bb7ffe25f351685e806c9c7c69f451ba9
Jashan Chopra <jashanxchopra@gmail.com>
2019-05-22 10:06:42 -0600

    automatic slideshow needs work

--- website/JavaScript/slideshow.js
+++ website/JavaScript/slideshow.js
@@ -20,12 +20,15 @@ function showSlides(n) {
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (i = 0; i < slides.length; i++) {   // main slide movement for loop
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length; i++) {     // this is the activated dots
       dots[i].className = dots[i].className.replace(" active", "");
   }
+  // slideIndex++;
+  // if (slideIndex > slides.length) {slideIndex = 1}  // this should auto move slides
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  // setTimeout(showSlides, 4000);     // slides should change every 2 seconds
 }
