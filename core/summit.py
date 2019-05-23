57227b54fdc45c2c4b7ca8bd42b6f14ca3c6d212
Brendan Blanchard <brendano257@gmail.com>
2019-05-23 11:27:20 -0600

    Added dual_plot_methane() to sequence

--- core/summit.py
+++ core/summit.py
@@ -23,10 +23,11 @@ import sys
 from summit_core import processor_dirs
 
 for d in processor_dirs:
-	sys.path.append(str(d))
+    sys.path.append(str(d))
 
 from voc_main_loop import main as voc_processor
 from methane_main_loop import main as methane_processor
+from methane_main_loop import dual_plot_methane
 from picarro_main_loop import main as picarro_processor
 from error_main_loop import check_for_new_data, check_existing_errors
 from summit_daily import check_load_dailies as daily_processor
@@ -37,49 +38,52 @@ import asyncio
 
 
 async def main(logger):
+    errors = []  # initiate with no errors
 
-	errors = []  # initiate with no errors
+    while True:
+        """
+        Processors that are passed a logger will log to /core, others will log to their individual directories/files.
+        """
 
-	while True:
-		"""
-		Processors that are passed a logger will log to /core, others will log to their individual directories/files.
-		"""
+        dailies = await asyncio.create_task(daily_processor(logger))
+        if dailies:
+            daily_plots = await asyncio.create_task(plot_dailies(logger))
 
-		dailies = await asyncio.create_task(daily_processor(logger))
-		if dailies:
-			daily_plots = await asyncio.create_task(plot_dailies(logger))
+        vocs = await asyncio.create_task(voc_processor())
+        methane = await asyncio.create_task(methane_processor())
+        picarro = await asyncio.create_task(picarro_processor())
 
-		vocs = await asyncio.create_task(voc_processor())
-		methane = await asyncio.create_task(methane_processor())
-		picarro = await asyncio.create_task(picarro_processor())
+        if methane or picarro:
+            await asyncio.create_task(dual_plot_methane(logger))
 
-		if vocs or methane or picarro:
-			await asyncio.create_task(check_send_plots(logger))
+        if vocs or methane or picarro:
+            await asyncio.create_task(check_send_plots(logger))
 
-		errors = await asyncio.create_task(check_for_new_data(logger, active_errors=errors))
+        errors = await asyncio.create_task(check_for_new_data(logger, active_errors=errors))
 
-		if errors:
-			errors = await asyncio.create_task(check_existing_errors(logger, active_errors=errors))
+        if errors:
+            errors = await asyncio.create_task(check_existing_errors(logger, active_errors=errors))
 
-		print('Sleeping...')
-		for i in range(20):
-			await asyncio.sleep(30)
+        print('Sleeping...')
+        for i in range(20):
+            await asyncio.sleep(30)
 
 
 if __name__ == '__main__':
 
-	try:
-		from summit_core import methane_dir as rundir
-		from summit_core import configure_logger
-		logger = configure_logger(rundir, __name__)
+    try:
+        from summit_core import methane_dir as rundir
+        from summit_core import configure_logger
 
-	except Exception as e:
-		print(f'Error {e.args} prevented logger configuration.')
-		send_processor_email('MAIN', exception=e)
-		raise e
+        logger = configure_logger(rundir, __name__)
 
-	loop = asyncio.get_event_loop()
-	loop.create_task(move_log_files(logger))
-	loop.create_task(main(logger))
+    except Exception as e:
+        print(f'Error {e.args} prevented logger configuration.')
+        send_processor_email('MAIN', exception=e)
+        raise e
 
-	loop.run_forever()
+    loop = asyncio.get_event_loop()
+    loop.create_task(move_log_files(logger))
+    loop.create_task(main(logger))
+
+    loop.run_forever()
