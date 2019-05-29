fa8c72ad7d5d6c82c8718fdb753f9c780c60a28b
Jashan Chopra <jashanxchopra@gmail.com>
2019-05-29 11:39:44 -0600

    created same residual plots for the acetylene methane ratio

--- analyses/HarmonicFit/aceMethanePlotting.py
+++ analyses/HarmonicFit/aceMethanePlotting.py
@@ -1,34 +1,34 @@
 """
-Created on March 28th, 2019. This script works on preliminary plotting data from the CCGVU exported functions,
+Created on March 29th, 2019. This script works on preliminary plotting data from the CCGVU exported functions,
 residuals, and other features in the Python environment.
 
 This data (from test1.txt) is from CCGVU with the DEFAULT settings. This is three polynomial terms, and four
 harmonic terms, with no switching in longterm or shortterm settings on the fast fourier transform
 """
 
-# Import libraries
+# Import Libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
-# TODO: Import Seaborn and look at those plotting features?
 
-# Reading in file
-root = r'C:\Users\ARL\Desktop\J_Summit\analyses\HarmonicFit'                            # Root File Location
-data = pd.read_csv(root + r'\test1.txt', encoding='utf8', delim_whitespace=True)        # whitespace delimitter read
-data = data.dropna(axis=1, how='any')                                                   # drop NaN columns
+# Reading in File
+root = r'C:\Users\ARL|Desktop\J_Summit\analyses\HarmonicFit'
+data = pd.read_csv(root + r'\aceDefault.txt', encoding='utf8', delim_whitespace=True)
+data = data.dropna(axis=1, how='any')
 
-# Printing for Data Understanding
+# Printing for Data Verification
 pd.set_option('display.max_columns', None)
 print(data.columns)
+print('-'*10)
+print(data.head)
 
 # Exploratory Graphing
-# TODO: Might be nice to display these different scatterplots as subplots seperately, than show overlayed
 plt.figure(1)
 plt.plot(data['date'], data['value'], '.', alpha=0.5, label='Original Data')
 plt.plot(data['date'], data['function'], linewidth=2, alpha=0.8, label='Fitted Function')
 plt.plot(data['date'], data['smooth'], linewidth=2, label='Smoothed Fitted Function')
-plt.title('Ethane / Methane Ratio')
+plt.title('Acetylene / Methane Ratio')
 plt.xlabel('Decimal Year')
 plt.ylabel('Mixing Ratio [ppb]')
 plt.legend()
@@ -61,7 +61,7 @@ data.insert(1, 'DOY', doy)                                                      
 plt.figure(3)
 plt.plot(data['DOY'], data['residuals'], '.', alpha=0.8, label='Normal Residuals')
 plt.plot(data['DOY'], data['resid_smooth'], '.', alpha=0.2, label='Residuals from Smoothed Fit')
-plt.title('Fitted Function Residuals')
+plt.title('Daily Residuals in Acetylene / Methane Ratio')
 plt.xlabel('Decimal Year')
 plt.ylabel('Mixing Ratio [ppb]')
 plt.legend()
