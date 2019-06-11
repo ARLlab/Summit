34bd508ed9b695d40f1251d9767691f0b26cbd9c
Jashan Chopra <jashanxchopra@gmail.com>
2019-06-11 11:38:37 -0600

    finished picarro data trimming, output files for ccg tool

--- analyses/HarmonicFit/genFuncs/picarroProducts.py
+++ analyses/HarmonicFit/genFuncs/picarroProducts.py
@@ -8,13 +8,11 @@ sort these files comes from Brendan Blanchard
 # import libraries
 import pandas as pd
 import os
-import timeit
 import numpy as np
 
 
 def picarroProd():
 
-    start_time = timeit.default_timer()
     # import data
     colnames = ['date', 'na', 'na1', 'na2', 'julian', 'na3', 'na4', 'status', 'na5', 'na6', 'na7', 'na8', 'na9', 'MPV',
                 'na10', 'co', 'na11', 'co2', 'na12', 'ch4', 'na13']                 # temp column names
@@ -37,13 +35,43 @@ def picarroProd():
             picarro.append(data.values)                                             # append array vals to list
 
     picarro_comb = np.vstack(picarro)                                               # numpy vertical stack to concat
+
     dataFinal = pd.DataFrame(picarro_comb)                                          # turn back to DF
 
     dataFinal = dataFinal.dropna(axis=0, how='any')                                 # drop NaN values
     dataFinal.columns = [x for x in colnames if x not in badcols]                   # reset column names
-    dataFinal = dataFinal[dataFinal['MPV'] == 963]                                  # only 963 MPV position
+    dataFinal = dataFinal[dataFinal['status'] == '963']                             # only 963 status
+    dataFinal = dataFinal[~(dataFinal['ch4'] == '07:15:55.000')]                    # remove one glitched value
 
-    print(timeit.default_timer() - start_time)
+    # add year to the julian day
+    year = dataFinal['date'][1]
+    year = float(year.split('-')[0])
+    dataFinal['julian'] = dataFinal['julian'].values.astype(float) / 365 + year
+
+    # create three dataframes for each file output
+    dataFinal_co = pd.concat([dataFinal['julian'], dataFinal['co'].astype(float)], axis=1)
+    dataFinal_co2 = pd.concat([dataFinal['julian'], dataFinal['co2'].astype(float)], axis=1)
+    dataFinal_ch4 = pd.concat([dataFinal['julian'], dataFinal['ch4'].astype(float)], axis=1)
+
+    # unit conversions to ppb
+    dataFinal_co['co'] = dataFinal_co['co'] * 1000
+    dataFinal_ch4['ch4'] = dataFinal_ch4['ch4'] * 1000
+
+    # transform into three different files
+    with open('picarro_co.txt', 'w+') as f:
+        for index, value in dataFinal_co.iterrows():
+            f.write('%f ' % value.julian)
+            f.write('%f\n' % value.co)
+
+    with open('picarro_co2.txt', 'w+') as f:
+        for index, value in dataFinal_co2.iterrows():
+            f.write('%f ' % value.julian)
+            f.write('%f\n' % value.co2)
+
+    with open('picarro_ch4.txt', 'w+') as f:
+        for index, value in dataFinal_ch4.iterrows():
+            f.write('%f ' % value.julian)
+            f.write('%f\n' % value.ch4)
 
 
 if __name__ == '__main__':
