ad7dc3d46926f060c3e508c32ff127d32dd09033
Jashan Chopra <jashanxchopra@gmail.com>
2019-06-13 15:37:15 -0600

    fixed an issue cutting out 2014 values due to merge reindexing prior 2012 values

--- analyses/Basic_Functions/noaaDates.py
+++ analyses/Basic_Functions/noaaDates.py
@@ -11,9 +11,10 @@ def noaaDateConv(dataframe):
     :return: the same dataframe with the datetime column replaced by year, month, day, hour, and minute
     """
 
-    year, month, day, hour, minute, cpd = [], [], [], [], [], []
-    cpd_name = dataframe.columns[1]
+    year, month, day, hour, minute, cpd = [], [], [], [], [], []                        # preallocate lists
+    cpd_name = dataframe.columns[1]                                                     # get the cpd name
 
+    # iterate through rows and append lists, seperating components of the datetime
     for index, value in dataframe.iterrows():
         year.append(value.datetime.year)
         month.append(value.datetime.month)
@@ -22,11 +23,17 @@ def noaaDateConv(dataframe):
         minute.append(value.datetime.minute)
         cpd.append(value[cpd_name])
 
+    # drop previous columns
     dataframe.drop(['datetime', cpd_name], axis=1, inplace=True)
+    dataframe = dataframe.reset_index()
+    dataframe.drop('index', axis=1, inplace=True)
+
+    # append each list to the new dataframe in appropriate order
     for item in [year, month, day, hour, minute, cpd]:
         item = pd.Series(item)
-        dataframe = dataframe.merge(item.to_frame(), left_index=True, right_index=True)
+        dataframe = dataframe.merge(item.to_frame(), left_index=True, right_index=True, how='inner')
 
+    # rename columns
     dataframe.columns = ['year', 'month', 'day', 'hour', 'minute', cpd_name]
 
     return dataframe
