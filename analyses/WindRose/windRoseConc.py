7ecc257b7ea9da7bd47853615fde471604132b1d
Jashan Chopra <jashanxchopra@gmail.com>
2019-06-18 15:30:02 -0600

    vastly improved wind rose plotting, fixed direction matching, improved readability and efficiency

--- analyses/WindRose/windRoseConc.py
+++ analyses/WindRose/windRoseConc.py
@@ -7,96 +7,49 @@ with various atmospheric tracer concentrations
 
 # import libraries and functions
 import matplotlib.pyplot as plt
-from WindRose.metTrim import metTrim
+from WindRose.metTrim import metTrim, createDatetime
 import pandas as pd
-import numpy as np
-from numba import njit
 import matplotlib.cm as cm
+import windrose
 
 
-@njit
-def fastDateCombThree(tolerence, date1, date2, value1, value2, value3):
-    """
-    fastDateCombThree takes values from a numpy array and combines data points with mutual dates (in decimal year)
-    within a specified tolerence (in days). The final size of the array will be the size of the smaller array. This
-    specific case uses three values as optimized for the windRose dataset, but it can easily be changed.
-
-    note: output values that are negative or zero for the date are considered nan and can be removed. The for loop
-    method is faster than typical vectorizing because numba usings a C codebase
-
-    :param tolerence: number of days where a value will be considered time equivalent with another
-    :param date1: the decimal year dates from the LARGER dataset
-    :param date2: the decimal year dates from the SMALLER dataset
-    :param value1: the larger dataset values
-    :param value2: the smaller dataset values
-    :param value3: optional third dataset if it is same size as value2 (saves a step with indexing later)
-    :return: returned value arrays and a singular date array to be combined outside of function
-    """
-    rem = tolerence / 365                                                       # tolerence day turned to dec
-    dates = np.empty(date2.shape)                                               # preallocate arrays
-    value_1 = np.empty(value2.shape)
-    value_2 = np.empty(value2.shape)
-    value_3 = np.empty(value3.shape)
-
-    for i in range(len(dates)):                                                 # loop over each datapoint
-        high = date2[i] + rem                                                   # upper date tolerence
-        low = date2[i] - rem                                                    # lower date tolerence
-        indices = (date1 <= high) & (date1 >= low)                              # larger data date points between tols
-        dirMed = np.nanmedian(value1[indices])                                  # median of these indices
-        if ~np.isnan(dirMed):                                                   # if median was not nan, update arrays
-            dates[i] = date2[i]
-            value_1[i] = dirMed
-            value_2[i] = value2[i]
-            value_3[i] = value3[i]
-
-    return dates, value_1, value_2, value_3
-
-
-def windRose():
+def metCombo(filename):
 
     # ---- import data
     met = metTrim()
     root = r'C:\Users\ARL\Desktop\J_Summit\analyses\HarmonicFit\textfiles'  # root source
-    ethane = pd.read_csv(root + r'\ethane.txt', encoding='utf8', delim_whitespace=True)
-    ace = pd.read_csv(root + r'\acetylene.txt', encoding='utf8', delim_whitespace=True)
+    sheet = pd.read_csv(filename, encoding='utf8', delim_whitespace=True)
+
+    # ---- data organization
+    colnames = ['yr', 'mo', 'day', 'hr', 'min', 'val']                                          # column names
+    sheet.columns = colnames                                                                # rename cols
+    sheet['datetime'] = createDatetime(sheet['yr'],                                         # create datetime
+                                       sheet['mo'],
+                                       sheet['day'],
+                                       sheet['hr'])
+    sheet.drop(['yr', 'mo', 'day', 'hr', 'min'], axis=1, inplace=True)                      # drop old cols
 
     # ---- trimming data
-    titles_old = ['date', 'value', 'residuals']
-    ace_titles_new = ['date_ace', 'resid_ace']
-    eth_titles_new = ['date_eth', 'resid_eth']
+    earlyVals = (met['datetime'] <= sheet['datetime'][0])                       # early met vals
+    met.drop(earlyVals, axis=0, inplace=True)                                   # trim early vals
+    met.reset_index(drop=True, inplace=True)                                    # reset index
+    met.drop(['steady'], axis=1, inplace=True)                                  # remove some columns
 
-    # drop unused columns for speed
-    ethDrop = ethane.drop(labels=(ethane.columns[(np.logical_and(ethane.columns != titles_old[0],
-                                                                 ethane.columns != titles_old[1]))]), axis=1)
-    aceDrop = ace.drop(labels=(ace.columns[(np.logical_and(ace.columns != titles_old[0],
-                                                           ace.columns != titles_old[1]))]), axis=1)
+    # merge the met data onto the concentration data by finding the nearest datetime within an hour
+    combo = pd.merge_asof(sheet, met, on='datetime',
+                          direction='nearest',
+                          tolerance=pd.Timedelta('1 hour'))
 
-    ethDrop.columns = eth_titles_new  # rename columns
-    aceDrop.columns = ace_titles_new
+    return combo
 
-    earlyVals = ~(met['DecYear'] <= ethDrop['date_eth'][0])                     # early met vals
-    met = met[earlyVals]                                                        # remove those vals
-    met = met.reset_index()                                                     # reset index
-    met = met.drop(['index', 'spd', 'steady'], axis=1)                          # remove some columns
 
-    metEthane = pd.concat([met, ethDrop], sort=False, axis=1)                   # combine datasets
-    metFull = pd.concat([metEthane, aceDrop], sort=False, axis=1)
-    metFull['date_ace'] = metFull['date_eth']                                   # ethane and ace dates same, drop one
-    metFull = metFull.drop(['date_ace'], axis=1)
+def windRose():
+    root = r'C:\Users\ARL\Desktop\J_Summit\analyses\HarmonicFit\textfiles'          # root source
+    ethPath = root + r'\ethane.txt'
+    acePath = root + r'\acetylene.txt'
 
-    dates, met, ethane, ace = fastDateCombThree(1, metFull['DecYear'].values,   # call fast date combine function
-                                                metFull['date_eth'].values,
-                                                metFull['dir'].values,
-                                                metFull['resid_eth'].values,
-                                                metFull['resid_ace'].values)
-
-    metFinal = pd.DataFrame(columns=['DecYear', 'dir', 'eth', 'ace'])           # preallocate DF
-    # insert values into final dataframe
-    metFinal['DecYear'], metFinal['dir'], metFinal['eth'], metFinal['ace'] = dates, met, ethane, ace
-
-    index = ~(metFinal <= 0)                                                    # remove negative vals and 0
-    metFinal = metFinal[index]
-    metFinal = metFinal.dropna(axis=0, how='any')                               # drop nan
+    ethane = metCombo(ethPath)
+    ace = metCombo(acePath)
 
     # ---- plotting
     # setup subplots
@@ -105,13 +58,13 @@ def windRose():
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.2, hspace=0)
 
     # setup ethane windrose
-    ax1.bar(metFinal['dir'].values, metFinal['eth'].values, normed=False, opening=0.9, edgecolor='black',
+    ax1.bar(ethane['dir'].values, ethane['val'].values, normed=False, opening=0.9, edgecolor='black',
             nsector=24, bins=14, cmap=cm.viridis_r, blowto=False)
     ax1.set_title('Summit Ethane Conc. Residual [ppb]')
     ax1.set_legend(loc=6)
 
     # setup acetylene windrose
-    ax2.bar(metFinal['dir'].values, metFinal['ace'].values, normed=False, opening=0.9, edgecolor='black',
+    ax2.bar(ace['dir'].values, ace['val'].values, normed=False, opening=0.9, edgecolor='black',
             nsector=24, bins=6, cmap=cm.viridis_r, blowto=False)
     ax2.set_title('Summit Ace Conc. Residual [ppb]')
     ax2.set_legend(loc=7)
