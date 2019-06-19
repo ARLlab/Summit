59d06f94fdb1210efff9373410fe400e80c8b999
Jashan Chopra <jashanxchopra@gmail.com>
2019-06-19 10:01:06 -0600

    simplified decimal year to datetime conversion function

--- analyses/Basic_Functions/decToDatetime.py
+++ analyses/Basic_Functions/decToDatetime.py
@@ -1,20 +1,14 @@
 """
 This custom function turns decimal date of years into a datetime format. If decyear is in the form with the year,
 entire the year parameter as anything, it will be overwritten
-:param: year: the year for the datetime
-:param: decyear: the decimal year. Can be either of the form year.xxxxx (ex: 2019.7283) or of the form day.xxx (ex:
-3.394)
+:param: decyear: the decimal year. Should be of the form year.xxxxx (ex: 2019.7283)
 """
 import datetime as dt
 from isleapyear import isleapyear
 import numpy as np
 
 
-def convToDatetime(year, decyear):
-
-    # if decyear is in the form of year.xxx, extract the year value
-    if np.any(decyear > 2000):
-        year = int((str(decyear))[:4])
+def convToDatetime(decyear):
 
     # if the decyear is nan, just return it as a nan
     if np.isnan(decyear):
@@ -22,16 +16,11 @@ def convToDatetime(year, decyear):
 
     else:
         # get the starting point from inputted year
+        year = int((str(decyear))[:4])
         start = dt.datetime(year - 1, 12, 31)
+        numdays = (decyear - year) * (365 + isleapyear(year))
 
-        # if datetime is entered with the year, subtract the year to get just the decimal and turn into days format
-        if decyear > 2000:
-            numdays = (decyear - year) * (365 + isleapyear(year))
-        # otherwise, entered decyear was the number of days
-        else:
-            numdays = decyear
-
-        # return the timedelta of the days added to start, remove the microseconds
+        # return the timedelta of the days added to start
         result = start + dt.timedelta(days=numdays)
 
         return result
