186953c771be0996721db2f87102fe48c3b0b1a1
Jashan Chopra <jashanxchopra@gmail.com>
2019-06-19 11:48:19 -0600

    updated methane data, improved plots, incorporated flask data

--- analyses/WindRose/windRoseMethane.py
+++ analyses/WindRose/windRoseMethane.py
@@ -11,121 +11,82 @@ seasonal cycle has been developed in the data.
 
 # import libraries and functions
 import matplotlib.pyplot as plt
-from WindRose.metTrim import metTrim
+from WindRose.metTrim import metTrim, createDatetime
+from decToDatetime import dateConv
+from WindRose.windRoseConc import metCombo
 import pandas as pd
-import numpy as np
-from numba import njit
 import matplotlib.cm as cm
 import windrose
 
 
-@njit
-def fastDateCombTwo(tolerence, date1, date2, value1, value2):
-    """
-    fastDateCombThree takes values from a numpy array and combines data points with mutual dates (in decimal year)
-    within a specified tolerence (in days). The final size of the array will be the size of the smaller array.
-
-    note: output values that are negative or zero for the date are considered nan and can be removed. Comments for
-    this function in more detail can be found in 'windRoseConc.py'
-
-    :param tolerence: number of days where a value will be considered time equivalent with another
-    :param date1: the decimal year dates from the LARGER dataset
-    :param date2: the decimal year dates from the SMALLER dataset
-    :param value1: the larger dataset values
-    :param value2: the smaller dataset values
-    :return: returned value arrays and a singular date array to be combined outside of function
-    """
-    rem = tolerence / 365
-    dates = np.empty(date2.shape)
-    value_1 = np.empty(value2.shape)
-    value_2 = np.empty(value2.shape)
-
-    for i in range(len(dates)):
-        high = date2[i] + rem
-        low = date2[i] - rem
-        indices = (date1 <= high) & (date1 >= low)
-        dirMed = np.nanmedian(value1[indices])
-        if ~np.isnan(dirMed):
-            dates[i] = date2[i]
-            value_1[i] = dirMed
-            value_2[i] = value2[i]
-
-    return dates, value_1, value_2
-
-
 def windRoseMethane():
-    pass
 
     # ---- import data
-    met = metTrim()                                                                             # met data
-    root = r'C:\Users\ARL\Desktop\J_Summit\analyses\HarmonicFit\textFiles'                      # root
-
-    arl = pd.read_csv(root + r'\methaneARL.txt', encoding='utf8', delim_whitespace=True)        # GC methane data
-    pic = pd.read_csv(root + r'\picarro_ch4.txt', delim_whitespace=True, encoding='utf8')       # picarro ch4 data
-    pic.columns = ['date', 'value']
-    colkeep = ['date', 'value']                                                                 # col to keep
-    arl = arl.drop(labels=(arl.columns[np.logical_and(arl.columns != colkeep[0],
-                                                      arl.columns != colkeep[1])]),
-               axis=1)                                                                      # drop all other cols
-    arl = arl.dropna(axis=0, how='any')                                                         # goodbye nan values
-
-    # ---- combining datasets (met and GC)
-    earlyVals = ~(met['DecYear'] <= arl['date'][0])                                             # early met vals
-    met = met[earlyVals]                                                                        # remove those vals
-    met = met.reset_index()                                                                     # reset index
-    met = met.drop(['index', 'spd', 'steady'], axis=1)                                          # remove some columns
-
-    metARL = pd.concat([met, arl], sort=False, axis=1)                                          # combine datasets
-    dates, direc, gc = fastDateCombTwo(1, metARL['DecYear'].values,                             # call fast func
-                                       metARL['date'].values,
-                                       metARL['dir'].values,
-                                       metARL['value'].values)
-    metFinal = pd.DataFrame(columns=['DecYear', 'dir', 'value'])                                # preallocate mat
-    metFinal['DecYear'], metFinal['dir'], metFinal['value'] = dates, direc, gc                  # append columns
-
-    index = ~(metFinal <= 0)                                                                    # remove neg & zero
-    metFinal = metFinal[index]
-    metFinal = metFinal.dropna(axis=0, how='any')                                               # drop nan
+    root = r'C:\Users\ARL\Desktop\J_Summit\analyses\HarmonicFit\textfiles'
+    met = metTrim()
+    arl = metCombo(root + r'\methane2019updated.txt')
+    arl.dropna(axis=0, how='any', inplace=True)
+    pic = pd.read_csv(root + r'\picarro_ch4.txt',
+                      delim_whitespace=True, encoding='utf8', error_bad_lines=False, header=None)
+    flask = pd.read_csv(root + r'\flask_ch4.txt', delim_whitespace=True, error_bad_lines=False, header=None)
 
     # ---- combining datasets (met and picarro)
-    earlyVals = ~(met['DecYear'] <= pic['date'][0])                                             # early date values
-    met = met[earlyVals]                                                                        # remove
-    met = met.reset_index()
+    pic.columns = ['date', 'value']
+    pic['datetime'] = dateConv(pic['date'].values)
+    pic.drop('date', axis=1, inplace=True)
+    earlyVals = ~(met['datetime'] <= pic['datetime'][0])
+    met.drop(earlyVals, axis=0, inplace=True)
+    met.reset_index(drop=True, inplace=True)
+    met.drop(['steady'], axis=1, inplace=True)
 
-    lateVals = ~(pic['date'] >= met['DecYear'].iloc[-1] )                        # late date value
-    pic = pic[lateVals]                                                                         # removal
-    pic = pic.reset_index()
+    # merge the met data onto the concentration data by finding the nearest datetime within an hour\
+    pic.dropna(axis=0, how='any', inplace=True)
+    picarro = pd.merge_asof(pic.sort_values('datetime'), met,
+                            on='datetime',
+                            direction='nearest',
+                            tolerance=pd.Timedelta('1 hour'))
+    picarro.dropna(axis=0, how='any', inplace=True)
 
-    metPic = pd.concat([met, pic], sort=False, axis=1)
-    dates, direc, gc = fastDateCombTwo(1, metPic['DecYear'].values,                             # call fast func
-                                       metPic['date'].values,
-                                       metPic['dir'].values,
-                                       metPic['value'].values)
+    # ---- combining datasets (flask and met)
+    met = metTrim()
+    colnames = ['yr', 'mo', 'dy', 'hr', 'val']
+    flask.columns = colnames
+    flask['datetime'] = createDatetime(flask['yr'], flask['mo'],
+                                       flask['dy'], flask['hr'])
+    flask.drop(['yr', 'mo', 'dy', 'hr'], axis=1, inplace=True)
+    earlyVals = (met['datetime'] <= flask['datetime'][0])
+    met.drop(earlyVals, axis=0, inplace=True)
+    met.reset_index(drop=True, inplace=True)
+    met.drop(['steady'], axis=1, inplace=True)
 
-    metFinal2 = pd.DataFrame(columns=['DecYear', 'dir', 'value'])                               # preallocate mat
-    metFinal2['DecYear'], metFinal2['dir'], metFinal2['value'] = dates, direc, gc               # append columns
-
-    index = ~(metFinal2 <= 0)                                                                   # remove neg & zero
-    metFinal2 = metFinal2[index]
-    metFinal2 = metFinal2.dropna(axis=0, how='any')                                             # drop nan
-    metFinal2 = metFinal2.reset_index()
+    # merge the met data onto the concentration data by finding the nearest datetime within an hour
+    flaskMet = pd.merge_asof(flask, met, on='datetime',
+                             direction='nearest',
+                             tolerance=pd.Timedelta('1 hour'))
+    flaskMet.dropna(axis=0, how='any', inplace=True)
 
     # ---- plotting
-    fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw=dict(projection='windrose'))
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, subplot_kw=dict(projection='windrose'))
     fig.suptitle('Methane Conc. at Summit by Wind Direction', fontsize=16)
-    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.2, hspace=None)
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.2, hspace=-0.3)
 
     # setup GC methane windrose
-    ax1.bar(metFinal['dir'].values, metFinal['value'].values, normed=False, opening=0.9,
+    ax1.bar(arl['dir'].values, arl['val'].values, normed=False, opening=0.9,
             edgecolor='black', nsector=24, bins=14, cmap=cm.viridis_r, blowto=False)
-    ax1.set_title('GCFID Methane Conc. [ppb]')
-    ax1.set_legend(loc=6)
+    ax1.set_title('GCFID Methane Conc. [ppb]\n')
+    ax1.set_legend(loc=8, fancybox=True, shadow=True, bbox_to_anchor=(0.5, -1.05))
 
     # setup picarro methane windrose
-    ax2.bar(metFinal2['dir'].values, metFinal2['value'].values, normed=False, opening=0.9,
+    ax2.bar(picarro['dir'].values, picarro['value'].values, normed=False, opening=0.9,
             edgecolor='black', nsector=24, bins=14, cmap=cm.viridis_r, blowto=False)
-    ax2.set_title('Picarro Methane Conc. [ppb]')
-    ax2.set_legend(loc=7)
+    ax2.set_title('Picarro Methane Conc. [ppb]\n',)
+    ax2.set_legend(loc=8, fancybox=True, shadow=True, bbox_to_anchor=(0.5, -1.05))
+
+    # setup flask methane windrose
+    ax3.bar(flaskMet['dir'].values, flaskMet['val'].values, normed=False, opening=0.9,
+            edgecolor='black', nsector=24, bins=14, cmap=cm.viridis_r, blowto=False)
+    ax3.set_title('Flask Methane Conc. [ppb]\n')
+    ax3.set_legend(loc=8, fancybox=True, shadow=True, bbox_to_anchor=(0.5, -1.05))
 
     plt.show()
 
